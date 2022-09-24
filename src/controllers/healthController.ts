0d119473a5c131e61585f7d417dcf80c6ec4ad67
alyson <consulialyson@gmail.com>
2022-09-24 12:55:33 -0300

    fix: delete health router

--- src/controllers/healthController.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const appHealth = async (req: Request, res: Response) => {
-  res.sendStatus(200);
-};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import "../config/setup.js";
 
-import healthRouter from "./healthRouter.js";
 import authRouter from "./authRouter.js";
 import betRouter from "./betsRouter.js";
 import gameRouter from "./gamesRouter.js";
 import userRouter from "./usersRouter.js";
 
 const router = Router();
-router.use(healthRouter);
 router.use(authRouter);
 router.use(betRouter);
 router.use(gameRouter);
